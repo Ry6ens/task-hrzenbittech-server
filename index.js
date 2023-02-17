6447ae1a5bb34cb11617c4e519f831b44655b85e
Sergey Gulaga <ry6ens@gmail.com>
2023-02-17 21:41:30 +0200

    add cors

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ dotenv.config();
 
 const { PORT = 4000 } = process.env;
 
+const corsOrigin = {
+  origin: "https://task-hrzenbittech.vercel.app/",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOrigin));
 app.use(express.json());
 
 app.use("/api/contacts", contactRoutes);
