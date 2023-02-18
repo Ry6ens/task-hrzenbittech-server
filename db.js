be4d7293b55bee57899011b72f9653430499fa9e
Sergey Gulaga <ry6ens@gmail.com>
2023-02-18 13:07:56 +0200

    updt

--- db.js
+++ db.js
@@ -10,15 +10,19 @@ const { CLEARDB_DATABASE_URL } = process.env;
 
 const db = mysql.createPool({
   connectionLimit: 10,
-  host: "b1e290b6@eu-cdbr-west-03.cleardb.net",
+  host: "eu-cdbr-west-03.cleardb.net",
   user: "bceb8c703954ae",
   password: "b1e290b6",
   database: "heroku_bcf588308abb006",
 });
 
-db.connect((err) => {
+// db.connect((err) => {
+//   if (err) throw err;
+//   console.log("Database connected");
+// });
+
+db.getConnection((err) => {
   if (err) throw err;
   console.log("Database connected");
 });
-
 module.exports = { db };
