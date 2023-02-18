9f59b0b4012d35b751253c018d2213aa2f398072
Sergey Gulaga <ry6ens@gmail.com>
2023-02-18 13:41:16 +0200

    change dg

--- db.js
+++ db.js
@@ -6,15 +6,15 @@ dotenv.config();
 const { CLEARDB_DATABASE_URL } = process.env;
 
 // const db = mysql.createConnection(CLEARDB_DATABASE_URL);
-// const db = mysql.createPool(CLEARDB_DATABASE_URL);
+const db = mysql.createPool(CLEARDB_DATABASE_URL);
 
-const db = mysql.createPool({
-  connectionLimit: 10,
-  host: "eu-cdbr-west-03.cleardb.net",
-  user: "bceb8c703954ae",
-  password: "b1e290b6",
-  database: "heroku_bcf588308abb006",
-});
+// const db = mysql.createPool({
+//   connectionLimit: 10,
+//   host: "eu-cdbr-west-03.cleardb.net",
+//   user: "bceb8c703954ae",
+//   password: "b1e290b6",
+//   database: "heroku_bcf588308abb006",
+// });
 
 // db.connect((err) => {
 //   if (err) throw err;
